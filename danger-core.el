3450b9fdd761e1cb599e50ece6cfe503ad8efcf9
Heli Wang <heli.h101@gmail.com>
2015-06-19 11:01:54 -0700

    ocaml mode

--- danger-core.el
+++ danger-core.el
@@ -378,4 +378,36 @@
 ;;; Haskell Mode
 (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Ocaml Mode
+(setq auto-mode-alist
+       (append '(("\\.ml[ily]?$" . tuareg-mode)
+                 ("\\.topml$" . tuareg-mode))
+               auto-mode-alist))
+;; Setup environment variables using opam
+(dolist (var (car (read-from-string (shell-command-to-string "opam config env --sexp"))))
+  (setenv (car var) (cadr var)))
+;; Update the emacs path
+(setq exec-path (append (parse-colon-path (getenv "PATH"))
+                        (list exec-directory)))
+;; Update the emacs load path
+(add-to-list 'load-path (expand-file-name "../../share/emacs/site-lisp"
+                                          (getenv "OCAML_TOPLEVEL_PATH")))
+;; Automatically load utop.el
+(autoload 'utop "utop" "Toplevel for OCaml" t)
+(add-hook 'tuareg-mode-hook 'utop)
+
+(setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
+(add-to-list 'load-path "/Users/WangHeli/.opam/system/share/emacs/site-lisp")
+(require 'ocp-indent)
+(require 'merlin)
+
+(add-hook 'tuareg-mode-hook 'merlin-mode)
+(setq merlin-use-auto-complete-mode t)
+(setq merlin-error-after-save nil)
+;; minor mode waiting for feature release
+;; (autoload 'utop-minor-mode "utop" "Minor mode for utop" t)
+;; (add-hook 'tuareg-mode-hook 'utop-minor-mode)
+
+
 (provide 'danger-core)
